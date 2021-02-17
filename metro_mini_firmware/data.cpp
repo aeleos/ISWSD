efce71a0794803c977c53e86b8ec10bdcbceba71
Andrew <abalogh@u.rochester.edu>
2021-02-17 16:11:24 -0500

    added pressure sensor datasheet and updated the data files

--- metro_mini_firmware/data.cpp
+++ metro_mini_firmware/data.cpp
@@ -6,5 +6,26 @@ Dataset::Dataset(uint8_t type, uint8_t zero){
 }
 
 void Dataset::setup(){
-  Dataset::filename = String(Dataset::zero) + '.csv'
+  Dataset::filename = String(Dataset::zero) + '.csv';
+  
+  return;
+}
+
+void set_zero(float x, float y, float pressure){
+  Dataset::zero_pressure = pressure;
+  Dataset::x_coordinate[0] = x;
+  Dataset::y_coordinate[0] = y;
+  return;
+}
+
+float get_zero_pressure(void){
+  return Dataset::zero_pressure;
+}
+
+void record_measurement(float x, float y, float meas){
+  Dataset::x_coordinate[dp] = x;
+  Dataset::y_coordinate[dp] = y;
+  Dataset::measurements[dp] = meas * 3.28084;
+  dp++;
+  return;
 }
--- metro_mini_firmware/data.h
+++ metro_mini_firmware/data.h
@@ -9,13 +9,16 @@ class Dataset
   public:
     Dataset(uint8_t type, uint8_t zero); //start a new dataset object after every zero
     void setup();
-    void save(); // write the data to the SD card before erasing every zero
+    void set_zero(float x, float y, float pressure);
+    float get_zero_pressure(void);
+    void record_measurement(float x, float y, float meas);
   private:
-    uint8_t type,zero;
-    int zero_pressure;
+    uint8_t dp = 1; // datapoints taken
+    uint8_t type,zero; // for constructor
+    int zero_pressure; // pressure in hPa at the zero point
     char filename[7]; // name to which to write, based on which zero number this is
     float x_coordinate[50], y_coordinate[50]; // array of gps coordinates
-    uint16_t measurements[50] = {0}; // elevation change
+    float measurements[50] = {0}; // elevation change
     
 };
 
