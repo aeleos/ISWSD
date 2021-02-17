fc14a1ccee32751f91fc5bcf52efc6c232d30b01
Andrew <abalogh@u.rochester.edu>
2021-02-16 23:12:49 -0500

    small updates to the data structure

--- metro_mini_firmware/data.cpp
+++ metro_mini_firmware/data.cpp
@@ -1,1 +1,10 @@
+#include "data.h"
 
+Dataset::Dataset(uint8_t type, uint8_t zero){
+  Dataset::zero = zero;
+  Dataset::type = type;
+}
+
+void Dataset::setup(){
+  Dataset::filename = String(Dataset::zero) + '.csv'
+}
--- metro_mini_firmware/data.h
+++ metro_mini_firmware/data.h
@@ -7,13 +7,16 @@
 class Dataset
 {
   public:
-    Dataset(int type); //start a new dataset object after every zero
-    ~ Dataset();
+    Dataset(uint8_t type, uint8_t zero); //start a new dataset object after every zero
+    void setup();
     void save(); // write the data to the SD card before erasing every zero
   private:
+    uint8_t type,zero;
+    int zero_pressure;
     char filename[7]; // name to which to write, based on which zero number this is
     float x_coordinate[50], y_coordinate[50]; // array of gps coordinates
     uint16_t measurements[50] = {0}; // elevation change
+    
 };
 
 #endif
