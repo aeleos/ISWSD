7bcca701376c39f9ade8b2411cd2462951de24b1
Oliver <ogeller@u.rochester.edu>
2021-03-08 19:05:46 -0500

    Undo last commit

--- metro_mini_firmware/pinout.h
+++ metro_mini_firmware/pinout.h
@@ -1,9 +1,9 @@
 #define CD_PIN 9
 #define BAT_PIN 3
 #define ME_PIN 2
-#define ZE_PIN 4 
+#define ZE_PIN 4
 #define YES_PIN 2
-#define NO_PIN 4 
+#define NO_PIN 4
 #define SS_PIN 10
 
 #define PIN_DB 1000
