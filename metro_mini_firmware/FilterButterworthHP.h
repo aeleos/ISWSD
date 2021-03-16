ffec941f1a5987d1a9cb27d9eb11a9e05050b0e9
Carter <andrew@blueskiestravel.com>
2021-03-16 13:05:59 -0400

    Tuned the HPF

--- metro_mini_firmware/FilterButterworthHP.h
+++ metro_mini_firmware/FilterButterworthHP.h
@@ -1,11 +1,11 @@
 // Based on the filter class design from http://www.schwietering.com/jayduino/filtuino/index.php
 
-// For calculations: find alpha as ALPHA = dt/(dt + 1/(2*pi*fc)) where dt is the sampling rate and fc is the cutoff frequency. 
+// For calculations: find alpha = dt/(dt + 1/(2*pi*fc)) where dt is the sampling rate and fc is the cutoff frequency. 
 
-// Current Assumptions: dt = .001 and fc = .5Hz , 
+// Current Assumptions: dt = .1s and fc = .1Hz , 
 
 
-//High pass butterworth filter order=2 alpha1=0.003 
+//High pass butterworth filter order=2 alpha1=0.059 
 class  FilterBuHp2
 {
   public:
@@ -22,9 +22,9 @@ class  FilterBuHp2
     {
       v[0] = v[1];
       v[1] = v[2];
-      v[2] = (9.867597804394033778e-1 * x)
-         + (-0.97369487197631476860 * v[0])
-         + (1.97334424978129874262 * v[1]);
+      v[2] = (7.690341116075309902e-1 * x)
+         + (-0.59214452645912030171 * v[0])
+         + (1.48399191997100388107 * v[1]);
       return 
          (v[0] + v[2])
         - 2 * v[1];
