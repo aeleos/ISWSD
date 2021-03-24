5c4528240778f4865ad45b82b3e76a928ae0abd4
Carter <andrew@blueskiestravel.com>
2021-03-24 18:44:25 -0400

    Debugged, as long as the serial port reads data the data viz and saving works

--- MATLAB Data Processing/Arduino_offload_plot.m
+++ MATLAB Data Processing/Arduino_offload_plot.m
@@ -2,8 +2,9 @@ clear all;
 clc;
 clear workspace;
 
-path = '';
-port = '/dev/tty.usbmodem1434201';
+path = '';    % path to where the data files should be saved
+port = '/dev/tty.usbmodem1434201'; % serial port
+REFRESH = 100; % data points before updating plot
 
 % Based on Matlab Script Provided for Laboratory 10 ECE 216
 % Prof. Thomas Howard
@@ -15,44 +16,55 @@ if ~isempty(instrfind)
     delete(instrfind);
 end
 
-serial_device = serialport(port, 'BaudRate', 115200);
+serial_device = serialport(port, 115200);
 
 fopen(serial_device);
 
 START_TIME = now;
-START_TIME_STRING = datetime(START_TIME,'ConvertFrom','datenum');
-START_FILE_NAME = path + START_TIME_STRING + '.csv';
+START_TIME_STRING = datestr(datetime(START_TIME,'ConvertFrom','datenum'));
+START_FILE_NAME = [path, START_TIME_STRING, '.csv'];
 
 data_file = fopen(START_FILE_NAME,'wt');
-fprintf(data_file,'Point,Pressure(hPa),Temp(C)');
+fprintf(data_file,'Point,Pressure(hPa),Temp(C)\n');
 
-fig = figure, hold on;
+pressure = zeros(REFRESH,1);
+temperature = zeros(REFRESH,1);
+points = zeros(REFRESH,1);
+
+fig = figure;
+tiledlayout(2,1);
 set(fig,'KeyPressFcn',@endloop);
-plot(point,pressure,point,temperature);
-title('Pressure and Temperature vs Time');
-xlabel('data point');
-ylabel('hPa, C');
-legend('Pressure','Temp');
 
-pressure = zeros(100,1);
-temperature = zeros(100,1);
-points = [1-100:100-100];
+p = nexttile;
+hold(p,'on');
+plot(p,points,pressure);
+title('Pressure');
+ylabel('hPa');
+
+t = nexttile;
+hold(t,'on');
+plot(points,temperature);
+title('Temperature');
+ylabel('C');
 
 global loop;
 loop = 1;
+loop_count = 0;
 
 % LOOP
 
-while (loop)
+for j=1:4
     
-    for i=1:100
+    for i=1:REFRESH
         pressure( i ) = fscanf(serial_device,'%f');
         temperature( i ) = fscanf(serial_device,'%f');
-        points( i ) = loop_points( i ) + 100;
-        fprintf(data_file,'%f,%f,%f\n', points(i),pressure(i),temperature(i));
+        points( i ) = i+loop_count;
+        fprintf(data_file,'%d,%f,%f\n', points(i),pressure(i),temperature(i));
     end
+    loop_count = loop_count + REFRESH;
     
-    plot(points,pressure,points,temperature);
+    plot(p,points,pressure,'r');
+    plot(t,points,temperature,'b');
 end
 
 
@@ -62,8 +74,8 @@ fclose(serial_device);
 fclose(data_file);
 
 END_TIME = now;
-END_TIME_STRING = datetime(END_TIME,'ConvertFrom','datenum');
-END_FILE_NAME = path + START_TIME_STRING + ' - ' + END_TIME_STRING + '.csv';
+END_TIME_STRING = datestr(datetime(END_TIME,'ConvertFrom','datenum'));
+END_FILE_NAME = [path, START_TIME_STRING, ' - ', END_TIME_STRING, '.csv'];
 
 
 movefile(START_FILE_NAME,END_FILE_NAME);
